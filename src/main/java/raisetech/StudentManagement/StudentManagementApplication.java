830352cfe755b702418ea32771d45a81780b2c84
s-kobayashi01 <k.satoru1026@gmail.com>
2025-12-17 18:05:00 +0900

    mapに名前と年齢を追加するためのコードを追加

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -3,19 +3,29 @@ package raisetech.StudentManagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
+    Map<String, String> student = new HashMap<>();
 
 	public static void main(String[] args) {
     // localhost:8080
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-  @GetMapping("/liverpoolFC")
-  public String liverpoolFC() {
-    return "You'll Never Walk Alone";
+  @GetMapping("/student")
+  public String getStudentName() {
+    return student.toString();
   }
+
+  @PostMapping("/studentInfo")
+  public void setStudentInfo(String name, String age) {
+    student.put(name,age);
+  }
+
 }
