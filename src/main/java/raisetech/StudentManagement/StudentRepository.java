183a556814c319b54be3828b12fbebf708936698
s-kobayashi01 <k.satoru1026@gmail.com>
2025-12-21 22:48:29 +0900

    StudentRepository.javaをè¿修正して追加 

--- src/main/java/raisetech/StudentManagement/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/StudentRepository.java
@@ -11,13 +11,14 @@ import java.util.List;
 public interface StudentRepository {
 
     @Select("SELECT * FROM student")
-   List<Student> searchByName();
+    List<Student> searchByName();
 
-    @Insert("INSERT student values(#{name}, #{age})")
+    @Insert("INSERT INTO student (name, age) values(#{name}, #{age})")
     void registerStudent(String name, int age);
 
     @Update("UPDATE student SET age = #{age} WHERE name = #{name}")
     void updateStudent(String name, int age);
+
 
     @Delete("DELETE FROM student WHERE name = #{name}")
     void deleteStudent(String name);
