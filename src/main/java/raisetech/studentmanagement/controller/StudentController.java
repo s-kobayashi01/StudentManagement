3e409827bbda48973583e9c1f6943f0b2bd28cad
s-kobayashi01 <k.satoru1026@gmail.com>
2026-01-12 11:44:38 +0900

    28_Thymeleafを使ったPOST処理の成果物をコミット

--- src/main/java/raisetech/studentmanagement/controller/StudentController.java
+++ src/main/java/raisetech/studentmanagement/controller/StudentController.java
@@ -3,12 +3,17 @@ package raisetech.studentmanagement.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import raisetech.studentmanagement.controller.converter.StudentConverter;
 import raisetech.studentmanagement.data.Student;
 import raisetech.studentmanagement.data.StudentsCourses;
+import raisetech.studentmanagement.domain.StudentDetail;
 import raisetech.studentmanagement.service.StudentService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -32,7 +37,38 @@ public class StudentController {
     }
 
     @GetMapping("/studentsCourseList")
-    public List<StudentsCourses> getStudentsCourseList() {
-        return service.searchStudentsCourseList();
+    public String getStudentsCourseList(Model model) {
+        model.addAttribute("studentList", service.searchStudentsCourseList());
+        return "studentsCourseList";
+    }
+
+    @GetMapping("/newStudent")
+    public String newStudent(Model model) {
+        StudentDetail detail = new StudentDetail();
+        detail.setStudentsCourses(new ArrayList<>());
+        detail.getStudentsCourses().add(new StudentsCourses());
+        model.addAttribute("studentDetail", detail);
+        return "registerStudent";
+    }
+
+
+    @PostMapping("/registerStudent")
+    public String registerStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
+        if (result.hasErrors()) {
+            return "registerStudent";
+        }
+
+        service.create(studentDetail.getStudent(), studentDetail.getStudentsCourses());
+
+
+        //model.addAttribute("name", studentDetail.getStudent().getName());
+
+
+        //List<Student> student = new ArrayList<>();
+        //List<StudentDetail> studentDetails = new ArrayList<>();
+
+        //studentDetail.setStudent(student.get(name));
+        //System.out.println(studentDetail.getStudent().getName() + "さんが新規受講生として登録されました。");
+        return "redirect:/studentList";
     }
 }
